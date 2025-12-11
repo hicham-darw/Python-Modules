b5bb7cae89d3fcdbc4d001761e6d4792b3d24fb7
hicham-darw <hamoudhicham234@gmail.com>
2025-12-11 13:47:57 +0100

    ex000 ex01

---
+++ ex1/ft_plot_area.py
@@ -0,0 +1,8 @@
+def ft_plot_area():
+	print("Enter length: ")
+	length = int(input(""))
+	print("Enter Width: ")
+	width = int(input(""))
+	result = length * width
+	result_msg = "Plot area: " + str(result)
+	print (result_msg)
