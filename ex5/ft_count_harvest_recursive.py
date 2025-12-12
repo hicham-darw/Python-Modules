61a0a66d8f71ef60a25b44030cdbc52567b8b8f9
hicham-darw <hamoudihicham234@gmail.com>
2025-12-12 13:17:15 +0100

    ft_count_harvest iterative & recursive

---
+++ ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,18 @@
+days = 0
+
+inp = 0
+
+def ft_count_harvest_recursive():
+    global inp
+    global days
+
+    if (inp == 0):
+        days = int(input("Days until harvest: "))
+        inp += 1
+    if (days < int(inp)):
+        print("Harvest time!")
+        return ;
+    print("Day: ", inp);
+    n = int(inp)
+    inp += 1;
+    ft_count_harvest_recursive()
