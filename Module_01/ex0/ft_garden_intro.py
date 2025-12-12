a6db474c7724e435671d7ec1888bb1d6f47ffc7a
hicham-darw <hamoudihicham234@gmail.com>
2025-12-13 00:58:28 +0100

    start Module_01

---
+++ Module_01/ex0/ft_garden_intro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    name = "Rose"
+    height = "25cm"
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print("Plant:", name)
+    print("Height:", height)
+    print("Age:", 30, "days")
+    print("")
+    print("=== End of Program ===")
