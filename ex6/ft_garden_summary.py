1f85592fae4c60ac5cbc73d433d329a0cffc9a58
hicham-darw <hamoudihicham234@gmail.com>
2025-12-12 13:29:16 +0100

    garden summary

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,8 @@
+message = "Growing well!"
+
+def ft_garden_summary():
+    community = input("Enter name: ")
+    plants = input("Enter number of plants: ")
+    print("Garden: " + community)
+    print("Plants: ", plants)
+    print("Status: " + message )
