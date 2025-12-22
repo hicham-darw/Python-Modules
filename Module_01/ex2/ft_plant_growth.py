64a5ce1d9fa54c85536836a5a56135aac18769be
hicham-darw <hamoudihicham234@gmail.com>
2025-12-22 15:35:39 +0100

    fix for push

--- Module_01/ex2/ft_plant_growth.py
+++ Module_01/ex2/ft_plant_growth.py
@@ -7,23 +7,24 @@ class Plant:
         self.a = age
         print(self.name, ": ", self.h, "cm, ", self.a, " days old", sep="")
 
-    def grow(self, day=1):
-        self.h += (day - 1)
+    def grow(self):
+        self.h += 6
 
-    def age(self, age=1):
-        self.a += age - 1
+    def age(self):
+        self.a += 6
 
-    def get_info(self, days=0):
+    def get_info(self):
         print(self.name, ": ", self.h, "cm, ", self.a, " days old", sep="")
-        if days == 7:
-            print("Growth this week: ", "+", (days - 1), "cm", sep="")
+
+    def week_of_growth(self):
+        self.grow()
+        self.age()
+        self.get_info()
+        print("Growth this week: ", "+", "6cm", sep="")
 
 
 if __name__ == "__main__":
     print("=== Day 1 ===")
     plant1 = Plant("Rose", 25, 30)
     print("=== Day 7 ===")
-    growth = 7
-    plant1.age(growth)
-    plant1.grow(growth)
-    plant1.get_info(7)
+    plant1.week_of_growth()
