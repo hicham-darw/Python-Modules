7aead0fe194e087d39db2726f665651f3e7bfbf4
hicham-darw <hamoudihicham234@gmail.com>
2025-12-23 02:28:17 +0100

    .

---
+++ Module_03/ex0/ft_command_quest.py
@@ -0,0 +1,18 @@
+import sys
+
+
+if len(sys.argv) == 1:
+    print("=== Command Quest ===")
+    print("No arguments provided!")
+    print(f"Program name: {sys.argv[0]}")
+    print(f"Total arguments: {len(sys.argv)}")
+    print("")
+elif len(sys.argv) > 1:
+    print(f"Program name: {sys.argv[0]}")
+    print(f"Arguments received: {len(sys.argv) - 1}")
+    i = 1
+    while (i < len(sys.argv)):
+        print(f"Argument {i}: {sys.argv[i]}")
+        i += 1
+    print(f"Total arguments: {len(sys.argv)}")
+    print("")
