9cd52d22646527e54af63b81d362631d80224dfa
hicham-darw <hamoudhicham234@gmail.com>
2026-01-03 08:13:17 +0100

    cotinue in TextProcessor class

--- Module_05/ex0/stream_processor.py
+++ Module_05/ex0/stream_processor.py
@@ -26,63 +26,85 @@ class NumericProcessor(DataProcessor):
         self.total_numbers = 0
         self.average = 0
         self.size = 0
-        print("Initializing Numeric Processor...")
 
     def process(self, data: int) -> str:
-        try:
-            int(data)
-            self.total_numbers += data
-            self.size += 1
-        except Exception:
-            return "Not valide"
+        if not self.validate(data):
+            raise ValueError("invalid data! must be list of numbers")
         else:
-            return "valide"
-    def fromat_output(self, result: str) -> str:
-        return (str)
-
+            format = f"Output: Processed {self.size} numeric values, sum={self.total_numbers}, avg={self.total_numbers / self.size}"
+            print(format)
+            self.format_output(format)
 
     def validate(self, data: int) -> bool:
-        pass
+        print("Validation: Numeric data verified")
+        if not isinstance(data, list):
+            return False
+        try:
+            for elem in data:
+                int(elem)
+                self.total_numbers += elem
+                self.size += 1
+        except Exception:
+            return False
+        return True
+    
+    def format_output(self, format) -> str:
+        return format
+
+
 
 class TextProcessor(DataProcessor):
     def __init__(self):
         self.characters = 0
         self.words = 0
 
-    def process(self, data: str) -> str:
-        pass
-
-    def fromat_output(self, result: str) -> str:
-        pass
-
+    def process(self, data: Any) -> str:
+        if not self.validate(data):
+            raise ValueError("Inavlid!, must be string")
+        else:
+            self.word = len(data.split())
+            for d in data.split():
+                self.characters += len(d)
+            self.format_output(format)
 
     def validate(self, data: str) -> bool:
-        pass
+        if isinstance(data, str):
+            return True
+        print("OK!!!!!!")
+        return False
+
+    def fromat_output(self, result: str) -> str:
+        format = f"Output: Processed text: {self.characters} characters, {self.words} words"
+        print(format)
 
 
 class LogProcessor(DataProcessor):
-    def process(self, data: str) -> str:
+    def __init__(self):
         pass
 
+    def process(self, data: Any) -> str:
+        pass
 
+    def validate(self, data: Any) -> bool:
+        pass
+    
     def fromat_output(self, result: str) -> str:
-        pass
-
-
-    def validate(self, data: str) -> bool:
         pass
 
 if __name__ == '__main__':
     print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===\n")
-    numeric_proc = NumericProcessor()
-    text_proc = TextProcessor()
-    log_proc = LogProcessor()
+    print("Initializing Numeric Processor...")
+    lst = [1, 2, 9, 4, 5]
+    print(f"Processing data: {lst}")
+    proc = NumericProcessor()
+    proc.process(lst)
 
-    lst = [1, 2, "hicham", 4, 5]
-    for num in lst:
-        if numeric_proc.process(num) != 'Valide':
-            print(f"Processing data: \"ERROR: not valide number\"")
-            break
+    print("")
+    print("Initializing Text Processor...")
+    print("Processing data: \"Hello Nexus World\"")
+    text = "Hello Nexus World"
+    proc = TextProcessor()
+    proc.process(text)
     
 
 
